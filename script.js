f7839a31f1009ed43b5a096a795eef33e8d19c38
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2023-10-20 02:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,29 +32,18 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	 for (let i = 0; i < encodedStr.length; i++) {
-    const char = encodedStr[i];
 
-		  if (lookup.hasOwnProperty(char)) {
-      // Add the corresponding decoded character to the result array
-      decodedArr.push(lookup[char]);
-    } else {
-      // If the character is not in the lookup table, keep it unchanged
-      decodedArr.push(char);
-    }
-  }
-	const decodedStr = decodedArr.join("");
+	str = str.toUpperCase();
 
-  return decodedStr;
+	let output = "";
+	for(let i=0; i < str.length; i++){
+
+		if(lookup[str[i]])
+	}
+	else{
+
+		output += str[i];
+	}
 }
-
-
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-console.log(rot13("SERR YBIR? NPPVBWBO")); // Output: "FREE LOVE? ACCIOSOBO"
-// Do not change this line
-window.rot13 = rot13;
+return output;
+}
